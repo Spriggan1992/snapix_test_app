57ae17d890dc2fa8d0b96bd9cebdd8b170e65d01
Roman <spriggan1992@gmail.com>
2020-10-21 09:23:35 +0800

    bloc authentication

---
+++ lib/bloc/authentication/authentication_bloc.dart
@@ -0,0 +1,20 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+
+part 'authentication_event.dart';
+part 'authentication_state.dart';
+
+class AuthenticationBloc
+    extends Bloc<AuthenticationEvent, AuthenticationState> {
+  AuthenticationBloc() : super(AuthenticationInitial());
+
+  @override
+  Stream<AuthenticationState> mapEventToState(
+    AuthenticationEvent event,
+  ) async* {
+    // TODO: implement mapEventToState
+  }
+}
---
+++ lib/bloc/authentication/authentication_event.dart
@@ -0,0 +1,27 @@
+part of 'authentication_bloc.dart';
+
+abstract class AuthenticationEvent extends Equatable {
+  const AuthenticationEvent([List props = const []]) : super(props);
+
+  @override
+  List<Object> get props => [];
+}
+
+class AppStarted extends AuthenticationEvent {
+  @override
+  String toString() => 'AppStarted';
+}
+
+class LoggedIn extends AuthenticationEvent {
+  final String token;
+
+  LoggedIn({@required this.token}) : super([token]);
+
+  @override
+  String toString() => 'LoggedIn { token: $token }';
+}
+
+class LoggedOut extends AuthenticationEvent {
+  @override
+  String toString() => 'LoggedOut';
+}
